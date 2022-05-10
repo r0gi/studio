52e609d23d7b07d32d68bfa29c3abb0a9f4f6124
Andrew Carr <andrew@foxglove.dev>
2022-05-10 10:56:43 +1000

    Fix link style on NodePlayground

--- packages/studio-base/src/panels/NodePlayground/index.tsx
+++ packages/studio-base/src/panels/NodePlayground/index.tsx
@@ -14,7 +14,7 @@
 import { useTheme, Spinner, SpinnerSize } from "@fluentui/react";
 import ArrowLeftIcon from "@mdi/svg/svg/arrow-left.svg";
 import PlusIcon from "@mdi/svg/svg/plus.svg";
-import { Box, Input, Link, Stack } from "@mui/material";
+import { Input, Link, Stack } from "@mui/material";
 import { Suspense, useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
@@ -140,6 +140,8 @@ const WelcomeScreen = ({ addNewNode }: { addNewNode: (code?: string) => void }) 
       <TextContent>
         Welcome to Node Playground! Get started by reading the{" "}
         <Link
+          color="primary"
+          underline="hover"
           onClick={(e) => {
             e.preventDefault();
             setHelpInfo({ title: "NodePlayground", content: helpContent });
@@ -379,7 +381,7 @@ function NodePlayground(props: Props) {
 
           <Stack flexGrow={1} overflow="hidden ">
             {selectedNodeId == undefined && <WelcomeScreen addNewNode={addNewNode} />}
-            <Box
+            <div
               style={{
                 flexGrow: 1,
                 width: "100%",
@@ -414,7 +416,7 @@ function NodePlayground(props: Props) {
                   />
                 )}
               </Suspense>
-            </Box>
+            </div>
             <Stack>
               <BottomBar
                 nodeId={selectedNodeId}
